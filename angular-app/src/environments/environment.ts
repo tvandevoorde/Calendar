061cb95c59dd661ebf18951393f184358d272a4d
Tom <tom.vandevoorde@gmail.com>
2025-01-29 19:51:21 +0100

    ignore

--- angular-app/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-import { EnvironmentModel } from './environment.model';
-
-export const environment : EnvironmentModel = {
-    production: false,
-    googleClientId: 'your-google-client-id',
-    apiUrl: 'http://your-production-url.com',
-  };
